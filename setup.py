1335579751f4ef2a90f44b416d4193171a1d129c
hellerve <veit.heller@student.htw-berlin.de>
2016-11-28 19:29:33 +0100

    remove long description

--- setup.py
+++ setup.py
@@ -2,23 +2,14 @@
 import os
 from setuptools import setup, find_packages
 
-longdescr = open(
-    os.path.join(
-        os.path.dirname(__file__),
-        'README.md'
-    )
-).read()
-
-
 setup(
     name='lens_bson',
     author='Veit Heller',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     url='https://github.com/port-zero/lens_bson',
-    download_url = 'https://github.com/port-zero/lens_bson/tarball/0.0.1',
+    download_url = 'https://github.com/port-zero/lens_bson/tarball/0.0.2',
     description='A BSON parser for lens',
-    long_description=longdescr,
     packages=find_packages('.'),
     install_requires=[
         "pygments",
